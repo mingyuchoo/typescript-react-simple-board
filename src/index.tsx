a5b7a1e22c609b0a67223a31bdbce9a7be089bf8
mingyuchoo <mingyuchoo@gmail.com>
2022-09-12 06:40:46 +0900

    added react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import '@/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
 import reportWebVitals from '@/reportWebVitals';
@@ -11,7 +12,9 @@ import { store } from '@/store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
