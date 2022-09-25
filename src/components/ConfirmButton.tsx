90a97d6c6ce355a52396f519a4b9abc6a7f2f535
mingyuchoo <mingyuchoo@gmail.com>
2022-09-25 16:17:24 +0900

    fixed

--- src/components/ConfirmButton.tsx
+++ src/components/ConfirmButton.tsx
@@ -1,21 +1,30 @@
 import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
+enum Stage {
+  ONE,
+  TWO,
+  THREE,
+}
 export function ConfirmButton() {
   const [title, setTitle] = useState('Delete');
+  const [stage, setStage] = useState(Stage.ONE);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setTitle('Delete');
+      setStage(Stage.ONE);
     }, 1500);
     return () => clearTimeout(timer);
-  }, []);
+  }, [title, stage]);
 
   const handler = () => {
     if (title == 'Delete') {
       setTitle('Confirm to delete');
+      setStage(Stage.TWO);
     } else if (title == 'Confirm to delete') {
       setTitle('Confirmed');
+      setStage(Stage.THREE);
     }
   };
 
